92c8d567afc1942177cc58c9c66fbe81dbcf66a9
anilyildiz <anil-yildiz@outlook.com.tr>
2024-11-15 08:10:42 +0300

    Many-to-Many Map Course to Student

--- src/main/java/com/anily/hibernatedemo/entity/Course.java
+++ src/main/java/com/anily/hibernatedemo/entity/Course.java
@@ -25,6 +25,12 @@ public class Course {
     @JoinColumn(name = "course_id")
     List<Review> reviews;
 
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "course_student",
+    joinColumns = @JoinColumn(name = "course_id"),
+    inverseJoinColumns = @JoinColumn(name = "student_id"))
+    List<Student> students;
+
     public Course() {
     }
 
@@ -64,11 +70,26 @@ public class Course {
         this.reviews = reviews;
     }
 
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
     public void addReview(Review review) {
         if (reviews == null) {
             reviews = new ArrayList<>();
         }
         reviews.add(review);
+    }
+
+    public void addStudent(Student student){
+        if (students == null){
+            students = new ArrayList<>();
+        }
+        students.add(student);
     }
 
     @Override
